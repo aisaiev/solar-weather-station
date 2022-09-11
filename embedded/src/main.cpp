0a745e26e0b044e2b21a34f6de96c5865378126b
Anton Isaev <aisaev@intetics.com>
2022-09-11 20:01:29 +0300

    Added embedded ESP8266 project

---
+++ embedded/src/main.cpp
@@ -0,0 +1,194 @@
+#include <Arduino.h>
+#include <ArduinoJson.h>
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#include <PubSubClient.h>
+#include <Wire.h>
+#include "Adafruit_SHT31.h"
+#include <Adafruit_BMP280.h>
+#include <Adafruit_Sensor.h>
+#include "sensors_data.cpp"
+#include "constants.h"
+
+Adafruit_BMP280 bmp;
+Adafruit_SHT31 sht31 = Adafruit_SHT31();
+
+WiFiClient espClient;
+PubSubClient mqttClient(espClient);
+
+void initMqttServer();
+void connectMqtt();
+void publishSensorsData();
+
+void setup()
+{
+    Serial.begin(SERIAL_BAUD);
+
+    bmp.begin(BMP_SENSOR_I2C_ADDRESS);
+    sht31.begin(SHT_SENSOR_I2C_ADDRESS);
+
+    Serial.println("");
+    Serial.println("Wi-Fi SSID " + String(WIFI_SSID));
+    Serial.println("Wi-Fi PSWD " + String(WIFI_PASSWORD));
+    Serial.print("Connecting to Wi-Fi");
+
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(1000);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("Connected to Wi-Fi " + String(WIFI_SSID));
+    Serial.println("IP address " + WiFi.localIP().toString());
+
+    WiFi.setAutoConnect(true);
+    WiFi.setAutoReconnect(true);
+    WiFi.persistent(true);
+
+    initMqttServer();
+}
+
+void loop()
+{
+    if (!mqttClient.connected())
+    {
+        connectMqtt();
+    }
+    mqttClient.loop();
+    
+    ESP.deepSleep(SLEEP_DURATION);
+}
+
+//=====================
+// SENSORS
+//=====================
+String getMcuName()
+{
+    return "ESP8266EX";
+}
+
+float getCpuFrequency()
+{
+    return 80.0;
+}
+
+float getRamUsageKb()
+{
+    int totalRam = 81920;
+    int ramUsage = totalRam - ESP.getFreeHeap();
+    return (float)ramUsage / 1000;
+}
+
+float getRamUsagePercent()
+{
+    int totalRam = 81920;
+    int ramUsage = totalRam - ESP.getFreeHeap();
+    return (float)ramUsage / totalRam * 100;
+}
+
+float getTemperature()
+{
+    return sht31.readTemperature();
+}
+
+float getHumidity()
+{
+    return sht31.readHumidity();
+}
+
+float getPressure()
+{
+    return bmp.readPressure() / 100.0F;
+}
+
+float getAltitude()
+{
+    return bmp.readAltitude(1013.25);
+}
+
+int getBatteryLevel()
+{
+    return (int)((analogRead(0) * 100) / 1024);
+}
+
+float getBatteryVoltage()
+{
+    return 4.2 * ((float)analogRead(0) / 1024.0);
+}
+
+bool getBatteryCharging()
+{
+    return digitalRead(14) == HIGH;
+}
+
+SensorsData getSensorsData()
+{
+    String mcu = getMcuName();
+    float cpuFrequency = getCpuFrequency();
+    float ramUsageKb = getRamUsageKb();
+    float ramUsagePercent = getRamUsagePercent();
+    float temperature = getTemperature();
+    float humidity = getHumidity();
+    float pressure = getPressure();
+    float altitude = getAltitude();
+    float batteryVoltage = getBatteryVoltage();
+    int batteryLevel = getBatteryLevel();
+    bool batteryCharging = getBatteryCharging();
+    
+    SensorsData data = { mcu, cpuFrequency, ramUsageKb, ramUsagePercent, temperature, humidity, pressure, altitude, batteryVoltage, batteryLevel, batteryCharging };
+
+    return data;
+}
+
+//=====================
+// MQTT
+//=====================
+void initMqttServer()
+{
+    mqttClient.setBufferSize(512);
+    mqttClient.setServer(MQTT_HOST, MQTT_PORT);
+}
+
+void connectMqtt()
+{
+    while (!mqttClient.connected())
+    {
+        Serial.println("Connecting to MQTT broker...");
+        if (mqttClient.connect(MQTT_CLIENT_ID, MQTT_USER, MQTT_PASSWORD))
+        {
+            Serial.println("MQTT Connected");
+            publishSensorsData();
+            Serial.println("MQTT data has been sent");
+        }
+        else
+        {
+            Serial.println("Unable connect to MQTT");
+            delay(5000);
+        }
+    }
+}
+
+void publishSensorsData()
+{
+    SensorsData data = getSensorsData();
+
+    DynamicJsonDocument json(512);
+    json["mcu"] = data.mcu;
+    json["cpuFrequency"] = data.cpuFrequency;
+    json["ramUsageKb"] = data.ramUsageKb;
+    json["ramUsagePercent"] = data.ramUsagePercent;
+    json["temperature"] = data.temperature;
+    json["humidity"] = data.humidity;
+    json["pressure"] = data.pressure;
+    json["altitude"] = data.altitude;
+    json["batteryVoltage"] = data.batteryVoltage;
+    json["batteryLevel"] = data.batteryLevel;
+    json["batteryCharging"] = data.batteryCharging;
+
+    String payload;
+    serializeJson(json, payload);
+    mqttClient.publish(MQTT_TOPIC, payload.c_str(), true);
+}
---
+++ embedded/src/sensors_data.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+
+struct SensorsData
+{
+    String mcu;
+    float cpuFrequency;
+    float ramUsageKb;
+    float ramUsagePercent;
+    float temperature;
+    float humidity;
+    float pressure;
+    float altitude;
+    float batteryVoltage;
+    int batteryLevel;
+    bool batteryCharging;
+};
