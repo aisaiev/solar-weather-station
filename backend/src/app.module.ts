4c1ea6e832414a1c16f03c7ac3bb27d1fb9d607a
Anton Isaev <aisaev@intetics.com>
2022-09-20 11:31:40 +0300

    Serve react app

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppConfigModule } from './config/app-config.module';
 import { SensorsDataModule } from './sensors-data/sensors-data.module';
 import { MqttGatewayModule } from './mqtt-gateway/mqtt-gateway.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [AppConfigModule, SensorsDataModule, MqttGatewayModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '../frontend/build'),
+    }),
+    AppConfigModule,
+    SensorsDataModule,
+    MqttGatewayModule,
+  ],
   controllers: [],
   providers: [],
 })
