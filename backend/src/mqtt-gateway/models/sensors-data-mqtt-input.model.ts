0f22458883b932a3210ecbc45db78e05de5a1fa7
Anton Isaiev <a.o.isaiev@gmail.com>
2023-11-05 12:03:42 +0200

    Changed database schema

--- backend/src/mqtt-gateway/models/sensors-data-mqtt-input.model.ts
+++ backend/src/mqtt-gateway/models/sensors-data-mqtt-input.model.ts
@@ -1,4 +1,4 @@
-import { IsBoolean, IsNumber, IsString } from 'class-validator';
+import { IsNumber, IsString } from 'class-validator';
 
 export class SensorsDataMqttInput {
   @IsString()
@@ -17,31 +17,19 @@ export class SensorsDataMqttInput {
   temperature: number;
 
   @IsNumber()
+  internalTemperature: number;
+
+  @IsNumber()
   humidity: number;
+
+  @IsNumber()
+  internalHumidity: number;
 
   @IsNumber()
   pressure: number;
 
   @IsNumber()
-  altitude: number;
-
-  @IsNumber()
-  uvLevel: number;
-
-  @IsNumber()
-  uvRisk: number;
-
-  @IsNumber()
-  uvIndex: number;
-
-  @IsNumber()
-  uvPower: number;
-
-  @IsNumber()
   illuminance: number;
-
-  @IsNumber()
-  rainAnalog: number;
 
   @IsNumber()
   batteryVoltage: number;
@@ -49,44 +37,31 @@ export class SensorsDataMqttInput {
   @IsNumber()
   batteryLevel: number;
 
-  @IsBoolean()
-  batteryCharging: boolean;
-
   constructor(data: {
     mcu: string;
     cpuFrequency: number;
     ramUsageKb: number;
     ramUsagePercent: number;
     temperature: number;
+    internalTemperature: number;
     humidity: number;
+    internalHumidity: number;
     pressure: number;
-    altitude: number;
-    uvLevel: number;
-    uvRisk: number;
-    uvIndex: number;
-    uvPower: number;
     illuminance: number;
-    rainAnalog: number;
     batteryVoltage: number;
     batteryLevel: number;
-    batteryCharging: boolean;
   }) {
     this.mcu = data.mcu;
     this.cpuFrequency = data.cpuFrequency;
     this.ramUsageKb = data.ramUsageKb;
     this.ramUsagePercent = data.ramUsagePercent;
     this.temperature = data.temperature;
+    this.internalTemperature = data.internalTemperature;
     this.humidity = data.humidity;
+    this.internalHumidity = data.internalHumidity;
     this.pressure = data.pressure;
-    this.altitude = data.altitude;
-    this.uvLevel = data.uvLevel;
-    this.uvRisk = data.uvRisk;
-    this.uvIndex = data.uvIndex;
-    this.uvPower = data.uvPower;
     this.illuminance = data.illuminance;
-    this.rainAnalog = data.rainAnalog;
     this.batteryVoltage = data.batteryVoltage;
     this.batteryLevel = data.batteryLevel;
-    this.batteryCharging = data.batteryCharging;
   }
 }
