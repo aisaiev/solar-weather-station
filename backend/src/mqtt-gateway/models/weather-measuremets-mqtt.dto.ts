81c7c6bc35e08a47c84977337bcf7370aef05062
Anton Isaiev <aisaiev@aisaiev.net>
2026-05-17 20:01:14 +0300

    refactor: simplify WeatherMeasurementsMqttDto and update CreateWeatherMeasurementRequest to use it

--- backend/src/mqtt-gateway/models/weather-measuremets-mqtt.dto.ts
+++ backend/src/mqtt-gateway/models/weather-measuremets-mqtt.dto.ts
@@ -1,92 +1,75 @@
-import { IsNumber, IsString } from 'class-validator';
+import { IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class WeatherMeasurementsMqttDto {
+    @IsOptional()
     @IsString()
     mcu: string;
 
+    @IsOptional()
     @IsNumber()
     cpuFrequency: number;
 
+    @IsOptional()
     @IsNumber()
     ramUsageKb: number;
 
+    @IsOptional()
     @IsNumber()
     ramUsagePercent: number;
 
+    @IsOptional()
     @IsNumber()
     temperature: number;
 
+    @IsOptional()
     @IsNumber()
     internalTemperature: number;
 
+    @IsOptional()
     @IsNumber()
     humidity: number;
 
+    @IsOptional()
     @IsNumber()
     internalHumidity: number;
 
+    @IsOptional()
     @IsNumber()
     pressure: number;
 
+    @IsOptional()
     @IsNumber()
     illuminance: number;
 
+    @IsOptional()
     @IsNumber()
     batteryVoltage: number;
 
+    @IsOptional()
     @IsNumber()
     batteryCurrent: number;
 
+    @IsOptional()
     @IsNumber()
     batteryPower: number;
 
+    @IsOptional()
     @IsNumber()
     batteryLevel: number;
 
+    @IsOptional()
     @IsNumber()
     solarPanelVoltage: number;
 
+    @IsOptional()
     @IsNumber()
     solarPanelCurrent: number;
 
+    @IsOptional()
     @IsNumber()
     solarPanelPower: number;
 
-    constructor(data: {
-        mcu: string;
-        cpuFrequency: number;
-        ramUsageKb: number;
-        ramUsagePercent: number;
-        temperature: number;
-        internalTemperature: number;
-        humidity: number;
-        internalHumidity: number;
-        pressure: number;
-        illuminance: number;
-        batteryVoltage: number;
-        batteryCurrent: number;
-        batteryPower: number;
-        batteryLevel: number;
-        solarPanelVoltage: number;
-        solarPanelCurrent: number;
-        solarPanelPower: number;
-    }) {
-        this.mcu = data.mcu;
-        this.cpuFrequency = data.cpuFrequency;
-        this.ramUsageKb = data.ramUsageKb;
-        this.ramUsagePercent = data.ramUsagePercent;
-        this.temperature = data.temperature;
-        this.internalTemperature = data.internalTemperature;
-        this.humidity = data.humidity;
-        this.internalHumidity = data.internalHumidity;
-        this.pressure = data.pressure;
-        this.illuminance = data.illuminance;
-        this.batteryVoltage = data.batteryVoltage;
-        this.batteryCurrent = data.batteryCurrent;
-        this.batteryPower = data.batteryPower;
-        this.batteryLevel = data.batteryLevel;
-        this.solarPanelVoltage = data.solarPanelVoltage;
-        this.solarPanelCurrent = data.solarPanelCurrent;
-        this.solarPanelPower = data.solarPanelPower;
+    constructor(data: Partial<WeatherMeasurementsMqttDto>) {
+        Object.assign(this, data);
     }
 }
--- backend/src/weather-measurements/dto/create-weather-measurement.request.ts
+++ backend/src/weather-measurements/dto/create-weather-measurement.request.ts
@@ -1,20 +1,5 @@
-export class CreateWeatherMeasurementRequest {
-    mcu: string;
-    cpuFrequency: number;
-    ramUsageKb: number;
-    ramUsagePercent: number;
-    temperature: number;
-    internalTemperature: number;
-    humidity: number;
-    internalHumidity: number;
-    pressure: number;
-    illuminance: number;
-    batteryVoltage: number;
-    batteryCurrent: number;
-    batteryPower: number;
-    batteryLevel: number;
-    solarPanelVoltage: number;
-    solarPanelCurrent: number;
-    solarPanelPower: number;
+import { WeatherMeasurementsMqttDto } from 'src/mqtt-gateway/models/weather-measuremets-mqtt.dto';
+
+export type CreateWeatherMeasurementRequest = WeatherMeasurementsMqttDto & {
     date: Date;
-}
+};
